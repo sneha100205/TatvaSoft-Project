7d9ca31b64b2efced84ad08ebe327e20b3c7f705
sneha100205 <snehachatwani36@gmail.com>
2025-05-28 20:15:32 +0530

    Add files via upload

---
+++ Services/BookService.cs
@@ -0,0 +1,29 @@
+﻿using BooksApi.Models;
+using System.Collections.Generic;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly List<Book> books = new List<Book>();
+
+        //To Add Book
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        //To Get All Books
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        //To Get Single Book
+        public Book? GetBookById(int id)
+        {
+            return books.Find(x => x.Id == id);
+        }
+    }
+}
+
